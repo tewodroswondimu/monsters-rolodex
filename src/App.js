11cd9218352f342ec51ffa7ad7fd13fbf99450c1
Tewodros Wondimu <tewodroswondimu@gmail.com>
2021-02-09 15:22:32 +0300

    4. Class vs Function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        { name: 'Frankestein'}, 
+        { name: 'Drakula'}, 
+        { name: 'Zombie'}
+      ]
+    }
+  }
+
+  render() {
+    return(
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Welcome { this.state.name } <br/>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <button onClick={() => this.setState({ name: 'Tewodros Wondimu'})}>Show Full Name</button>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+/* opted to use class instead of function
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
   );
 }
+*/
 
 export default App;
