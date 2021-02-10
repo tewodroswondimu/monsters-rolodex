e116e457860d0408c4042ac0d5ef6a8b0f2aee93
Tewodros Wondimu <tewodroswondimu@gmail.com>
2021-02-10 15:07:25 +0300

    Ready to deploy

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
@@ -19,24 +20,23 @@ class App extends Component {
     .then(users => this.setState({monsters: users}))
   }
 
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  }
+
   render() {
+    const { monsters, searchField } = this.state; // deconstructs the state
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return(
       <div className="App">
-        <input type='search' placeholder='search monsters' onChange={
-          e => this.setState({ searchField: e.target.value })
-        }/>
-        <CardList monsters={this.state.monsters} />
+        <h1> Monsters Rolodex </h1>
+        <SearchBox placeholder='search monsters' handleChange={this.handleChange}/>
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
 }
-
-/* opted to use class instead of function
-function App() {
-  return (
-    
-  );
-}
-*/
 
 export default App;
